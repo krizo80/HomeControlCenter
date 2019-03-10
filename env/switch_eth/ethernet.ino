d831727ab6b4817555d76823cb926a88f273cf2c
root <root@xbian.private>
2019-03-10 21:28:52 +0100

    arduino update

--- env/switch_eth/ethernet.ino
+++ env/switch_eth/ethernet.ino
@@ -1,17 +1,9 @@
 #include "etherShield.h"
 #include "ETHER_28J60.h"
+#include <avr/wdt.h> //should be in any adruino IDE
 
-
-static uint8_t mac[6] = {0x54, 0x55, 0x58, 0x10, 0x00, 0x24};   
-static uint8_t ip[4] = {192, 168, 1, 2};                       
-static uint16_t port = 80;                                     
-
-static uint8_t status;
-
-ETHER_28J60 e;
 
 #define TRAFO        A0
-
 #define GATE         9
 #define HEAT         8
 #define DOOR         7
@@ -20,12 +12,24 @@ ETHER_28J60 e;
 #define WATER2       4
 #define WATER3       3
 #define AUX          2
+#define OFFSET       2
 
-#define OFFSET  2
+//#define DEBUG
+
+static uint8_t mac[6] = {0x54, 0x55, 0x58, 0x10, 0x00, 0x24};   
+static uint8_t ip[4] = {192, 168, 1, 2};                       
+static uint16_t port = 80;                                     
+static uint8_t status;
+static unsigned long httpTime = 0;
+static unsigned long currentTime = 0;
+
+ETHER_28J60 e;
+void(* resetFunc) (void) = 0; //declare reset function @ address 0
 
 void setup()
 {  
-     delay(1000);
+     wdt_disable(); //always good to disable it, if it was left 'on' or you need init time
+     delay(500);
    
      pinMode(GATE, OUTPUT);
      pinMode(GATE1, OUTPUT);
@@ -34,7 +38,7 @@ void setup()
      pinMode(WATER1, OUTPUT);
      pinMode(WATER2, OUTPUT);
      pinMode(WATER3, OUTPUT);
-     pinMode(AUX, OUTPUT);
+     pinMode(AUX, INPUT);
      pinMode(TRAFO, OUTPUT);
     
      digitalWrite(GATE,LOW);
@@ -44,76 +48,95 @@ void setup()
      digitalWrite(WATER1, LOW);
      digitalWrite(WATER2, LOW);
      digitalWrite(WATER3, LOW);
-     digitalWrite(AUX, LOW);
+     digitalWrite(AUX, HIGH);
      digitalWrite(TRAFO, LOW);
     
      e.setup(mac, ip, port);
+     wdt_enable(WDTO_8S); //enable it, and set it to 8s
+     
+     //only for debug purpose
+#ifdef DEBUG     
+     Serial.begin(9600);      // open the serial port at 9600 bps:
+     Serial.println("Debug");
+#endif     
  }
  
  void loop()
  {
    char* params;
+   int   in;
    
+        
+   /* read input and update status */
+   //-------------------------------------------------------------------------   
+   in = digitalRead(AUX);
+   if (in == LOW)
+   {
+     status = status | (1 << (AUX-OFFSET));       
+#ifdef DEBUG        
+     Serial.println("enable");
+#endif   
+   }
+   else
+   {     
+      status = status & (~(1 << (AUX-OFFSET)));       
+#ifdef DEBUG        
+      Serial.println("disable");
+#endif         
+   }
+   //-------------------------------------------------------------------------   
+       
+   /* read http request and create response*/   
+   //-------------------------------------------------------------------------
    params = e.serviceRequest();
    
    if (params != NULL)
    {
+     //get timestamp - used to perform reset if http request cannot be handled
+     httpTime = millis();
+     
      if (strcmp(params, "gate0") == 0)
      {
-        e.print("OK");          
-        e.respond();           
+       e.print("OK");          
+       e.respond();           
        digitalWrite(GATE,HIGH);
        delay(500);
        digitalWrite(GATE,LOW);
      }
      else if (strcmp(params, "gate1") == 0)
      {
-        e.print("OK");          
-        e.respond();           
+       e.print("OK");          
+       e.respond();           
        digitalWrite(GATE1,HIGH);
        delay(500);
        digitalWrite(GATE1,LOW);
      }
      else if (strcmp(params, "door") == 0)
      {    
-        e.print("OK");          
-        e.respond();           
+       e.print("OK");          
+       e.respond();           
        digitalWrite(DOOR,HIGH);
        delay(2000);
        digitalWrite(DOOR,LOW);
      }
      else if (strcmp(params, "heat_on") == 0)
      {    
-        e.print("OK");          
-        e.respond();           
+       e.print("OK");          
+       e.respond();           
        digitalWrite(HEAT,HIGH);
        status = status | (1 << (HEAT-OFFSET));
      }
      else if (strcmp(params, "heat_off") == 0)
      {    
-        e.print("OK");          
-        e.respond();           
+       e.print("OK");          
+       e.respond();           
        digitalWrite(HEAT,LOW);
        status = status & (~(1 << (HEAT-OFFSET)));
      }
-     else if (strcmp(params, "aux_on") == 0)
-     {    
-        e.print("OK");          
-        e.respond();           
-       digitalWrite(AUX,HIGH);
-       status = status | (1 << (AUX-OFFSET));       
-     }
-     else if (strcmp(params, "aux_off") == 0)
-     {    
-        e.print("OK");          
-        e.respond();           
-       digitalWrite(AUX,LOW);
-       status = status & (~(1 << (AUX-OFFSET)));       
-     }
      else if (strcmp(params, "water1_on") == 0)
      {    
-        e.print("OK");          
-        e.respond();           
+       e.print("OK");          
+       e.respond();           
        digitalWrite(WATER1,HIGH);
        digitalWrite(WATER2,LOW);
        digitalWrite(WATER3,LOW); 
@@ -124,8 +147,8 @@ void setup()
      }
      else if (strcmp(params, "water2_on") == 0)
      {    
-        e.print("OK");          
-        e.respond();           
+       e.print("OK");          
+       e.respond();           
        digitalWrite(WATER1,LOW);
        digitalWrite(WATER2,HIGH);
        digitalWrite(WATER3,LOW); 
@@ -136,8 +159,8 @@ void setup()
      }
      else if (strcmp(params, "water3_on") == 0)
      {    
-        e.print("OK");          
-        e.respond();           
+       e.print("OK");          
+       e.respond();           
        digitalWrite(WATER1,LOW);
        digitalWrite(WATER2,LOW);
        digitalWrite(WATER3,HIGH);  
@@ -148,8 +171,8 @@ void setup()
      }
      else if (strcmp(params, "water_off") == 0)
      {    
-        e.print("OK");          
-        e.respond();           
+       e.print("OK");          
+       e.respond();           
        digitalWrite(WATER1,LOW);
        digitalWrite(WATER2,LOW);
        digitalWrite(WATER3,LOW);       
@@ -160,11 +183,33 @@ void setup()
      }     
      else if (strcmp(params, "status") == 0)
      {    
-        e.print(status);          
-        e.respond();           
+       e.print(status);          
+       e.respond();           
      }          
    }
- 
+   //-------------------------------------------------------------------------
+   
+   //watchdog handling
+   //-------------------------------------------------------------------------
+   currentTime = millis();
+   
+   //timer overlap - reset timer
+   if (currentTime-httpTime < 0 )
+   {
+      httpTime = millis();
+      currentTime = millis();
+   }
+   
+   //on each 30min should appear at least on http request else reset system
+   if (currentTime-httpTime > 1000*60*30)
+   {
+     //call NULL function - dirty trick
+     resetFunc();
+   }   
+   
+   //reset watchdog - it prevents to infinity loop
+   wdt_reset();
+   //-------------------------------------------------------------------------   
  }
  
   

d25d9153825185334a130aadecfb69fb17c44df0
root <root@xbian.private>
2019-07-09 21:37:44 +0200

    new weather service

--- WeatherClass.py
+++ WeatherClass.py
@@ -34,19 +34,21 @@ class WeatherClass(object):
     # The class "constructor" - It's actually an initializer
     def __init__(self):
         self.__heater = {}
+
         
     def getCurrentWeather(self):
         weatherData = {}
 	try:
     	    with open(WeatherClass.__currWeatherFile) as f:
-        	data = json.load(f)
+    		data = json.load(f)
 
-    	    weatherData['icon'] = "https://openweathermap.org/img/w/"+data['weather'][0]['icon']+".png"
-    	    weatherData['temp'] = "%.1f" % data['main']['temp']
-    	    weatherData['date'] = datetime.fromtimestamp(data['dt']).strftime('%Y-%m-%d')
-    	    weatherData['time'] = datetime.fromtimestamp(data['dt']).strftime('%H:%M:%S')
-    	    weatherData['pressure'] = "%.1f" % data['main']['pressure']
-    	    weatherData['wind']= "%.1f" % data['wind']['speed']
+    	    weatherData['icon'] = "https://www.weatherbit.io/static/img/icons/"+data['data'][0]['weather']['icon']+".png"
+    	    weatherData['temp'] = "%.1f" % data['data'][0]['temp']
+	    datetime = data['data'][0]['ob_time']
+    	    weatherData['date'] = datetime[:datetime.find(" ")]
+    	    weatherData['time'] = datetime[datetime.find(" "):]
+    	    weatherData['pressure'] = "%.1f" % data['data'][0]['pres']
+    	    weatherData['wind']= "%.1f" % data['data'][0]['wind_spd']
     	    weatherData['wind_dir']=""
 	except:
 	    print "_____________weather exception1"
@@ -60,18 +62,19 @@ class WeatherClass(object):
     	    with open(WeatherClass.__hourlyWeatherFile) as f:
         	data = json.load(f)
         
-    	    for item in data['list']:
-        	id = id + 1
+    	    for item in data['data']:
         	# skip weather forecast between 1am to 5am, and display only temperature on every 3 hours
-        	if id > 2:
-            	    icon = "https://openweathermap.org/img/w/"+item['weather'][0]['icon']+".png"
-            	    time = datetime.fromtimestamp(item['dt']).strftime('%H:%M')
-            	    temp = "%.1f" % item['main']['temp']
-            	    wind = "%.1f" % item['wind']['speed']
-            	    presure = "%.1f" % item['main']['pressure']
-            	    weatherForecast.append(WeatherForecaset(id,time,temp,wind,presure,icon))
-            	    if id > 7:
-                	break
+        	if (id % 3) == 0:
+            	    icon = "https://www.weatherbit.io/static/img/icons/"+item['weather']['icon']+".png"
+		    time = item['timestamp_local']
+            	    time = time[time.find("T")+1:]
+            	    temp = "%.1f" % item['temp']
+            	    wind = "%.1f" % item['wind_spd']
+            	    presure = "%.1f" % item['pres']
+            	    weatherForecast.append(WeatherForecaset(id/3,time,temp,wind,presure,icon))
+        	id = id + 1
+		if id > 15:
+            	    break
 	except:
 	    print "_____________weather exception"
         return weatherForecast
@@ -81,22 +84,22 @@ class WeatherClass(object):
         id = 0
         weatherForecast = []
 	try:
-    	    with open(WeatherClass.__hourlyWeatherFile) as f:
+    	    with open(WeatherClass.__dailyWeatherFile) as f:
         	data = json.load(f)
         
-    	    for item in data['list']:
-		if (item['dt_txt'].find('12:00') != -1):
-        	    id = id + 1
-            	    icon = "https://openweathermap.org/img/w/"+item['weather'][0]['icon']+".png"
-            	    time = datetime.fromtimestamp(item['dt']).strftime('%Y-%m-%d')
-            	    temp = "%.1f" % item['main']['temp']
-            	    wind = "%.1f" % item['wind']['speed']
-            	    presure = "%.1f" % item['main']['pressure']
-            	    weatherForecast.append(WeatherForecaset(id,time,temp,wind,presure,icon))
-            	    if id > 2:
-                	break
+    	    for item in data['data']:
+        	id = id + 1
+            	icon = "https://www.weatherbit.io/static/img/icons/"+item['weather']['icon']+".png"
+		time = item['datetime']
+            	#time = time[time.find("T")+1:]
+		temp = "%.1f" % item['temp']
+            	wind = "%.1f" % item['wind_gust_spd']
+            	presure = "%.1f" % item['pres']
+            	weatherForecast.append(WeatherForecaset(id,time,temp,wind,presure,icon))
+            	if id > 2:
+            	    break
 	except:
-	    print "_____________weather exception"
+	    print "_____________weather exception_DAILY"
         return weatherForecast
 
 
@@ -118,6 +121,6 @@ class WeatherClass(object):
         if (files & WeatherClass.WeatherHourlyFile) <> 0:
             self.__saveWeatherFile( config.getHourlyWeatherForecastReq(), self.__hourlyWeatherFile)
 
-#        if (files & WeatherClass.WeatherDailyFile) <> 0:
-#            self.__saveWeatherFile( config.getDailyWeatherForecastReq(), self.__dailyWeatherFile)
+        if (files & WeatherClass.WeatherDailyFile) <> 0:
+            self.__saveWeatherFile( config.getDailyWeatherForecastReq(), self.__dailyWeatherFile)
 
